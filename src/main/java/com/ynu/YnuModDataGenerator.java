813a7385d0523ff3701c7eb014073fd956890240
“thirdh6o” <2514340934@qq.com>
2025-08-01 20:22:14 +0800

    init

---
+++ src/main/java/com/ynu/YnuModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.ynu;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class YnuModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
